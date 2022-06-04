49a368ad2259b8269abdb7d419097a4cbf84e144
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-06-03 21:45:36 -0700

    Minor adjustments and work to make responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,3 @@ var blinkingScroll = setInterval(function () {
     var scrollElement = document.getElementById('scroll');
     scrollElement.style.visibility = (scrollElement.style.visibility == 'hidden' ? '' : 'hidden');
 }, 750);
-
-const el = document.querySelector("#background");
